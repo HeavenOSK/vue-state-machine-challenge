0a4f3ce68d3a39bb1df59e0c769b289fca027ab5
HeavenOSK <heavenosk@gmail.com>
2024-05-04 23:37:26 +0900

    install storybook

---
+++ src/components/Toggle.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isSelected = ref(false)
+const toggle = () => (isSelected.value = !isSelected.value)
+</script>
+<template>
+  <div
+    class="h-8 w-12 bg-green-200 rounded-full shadow-lg relative cursor-pointer"
+    :class="{
+      'bg-green-400': isSelected
+    }"
+    @click="toggle"
+  >
+    <div
+      class="h-6 w-6 bg-white transition-transform duration-150 rounded-full absolute top-1 left-1"
+      :class="{ 'translate-x-4': isSelected }"
+    ></div>
+  </div>
+</template>
